ad3949af822a4616e351831c3f75ab811070da80
Antoine Fonck <antoinefonck98@gmail.com>
2019-08-12 15:08:45 +0200

    basic menu

--- src/main.c
+++ src/main.c
@@ -446,6 +446,8 @@ void		*trace_fractal(void *fractol)
 	return (NULL);
 }
 
+void		menu(t_fractol fractol);
+
 void		multithread(t_fractol fractol)
 {
 	t_fractol	params[NB_THREADS];
@@ -466,6 +468,7 @@ void		multithread(t_fractol fractol)
 	while (i--)
 		pthread_join(threads[i], NULL);
 	mlx_put_image_to_window(fractol.mlx.mlx_ptr, fractol.mlx.win_ptr, fractol.img.img_ptr, 0, 0);
+	menu(fractol);
 }
 
 void		init_mlx(t_mlx *mlx, char *title)
@@ -500,6 +503,50 @@ void		start_values(t_fractol *fractol, char *title)
 		fractol->frac = 0;
 }
 
+void	outputfrac(t_fractol fractol)
+{
+	if (fractol.frac == JULIA)
+		mlx_string_put(fractol.mlx.mlx_ptr, fractol.mlx.win_ptr, 100, 10, \
+					0xFFFFFF, "JULIA");
+	else if (fractol.frac == MANDELBROT)
+		mlx_string_put(fractol.mlx.mlx_ptr, fractol.mlx.win_ptr, 100, 10, \
+					0xFFFFFF, "MANDELBROT");
+	else if (fractol.frac == BURNING_SHIP)
+		mlx_string_put(fractol.mlx.mlx_ptr, fractol.mlx.win_ptr, 100, 10, \
+					0xFFFFFF, "BURNING SHIP");
+	else if (fractol.frac == BUFFALO)
+		mlx_string_put(fractol.mlx.mlx_ptr, fractol.mlx.win_ptr, 100, 10, \
+					0xFFFFFF, "BUFALLO");
+}
+
+void	outputcommands(t_fractol fractol)
+{
+	mlx_string_put(fractol.mlx.mlx_ptr, fractol.mlx.win_ptr, 10, 30,
+					0xFFFFFF, "Change      palette: N");
+	mlx_string_put(fractol.mlx.mlx_ptr, fractol.mlx.win_ptr, 75, 30,
+					fractol.color, "color");
+	mlx_string_put(fractol.mlx.mlx_ptr, fractol.mlx.win_ptr, 10, 50,
+					0xFFFFFF, "Iterations: O(+)/P(-) | current:");
+	mlx_string_put(fractol.mlx.mlx_ptr, fractol.mlx.win_ptr, 10, 70,
+					0xFFFFFF, "Zoom: scroll up/scroll down");
+	mlx_string_put(fractol.mlx.mlx_ptr, fractol.mlx.win_ptr, 10, 90,
+					0xFFFFFF, "Move: arrows");
+	mlx_string_put(fractol.mlx.mlx_ptr, fractol.mlx.win_ptr, 10, 110,
+					0xFFFFFF, "Reset: R");
+	mlx_string_put(fractol.mlx.mlx_ptr, fractol.mlx.win_ptr, 10, 130,
+					0xFFFFFF, "Change fractal: 1/2/3/4");
+	mlx_string_put(fractol.mlx.mlx_ptr, fractol.mlx.win_ptr, 10, 150,
+					0xFFFFFF, "Close the window: 'esc'");
+}
+
+void	menu(t_fractol fractol)
+{
+	outputfrac(fractol);
+	outputcommands(fractol);
+	mlx_string_put(fractol.mlx.mlx_ptr, fractol.mlx.win_ptr, 350, 50,
+					0xFFFFFF, ft_itoa(fractol.itmax));
+}
+
 int		main(int argc, char **argv)
 {
 	t_fractol	fractol;
@@ -522,7 +569,7 @@ int		main(int argc, char **argv)
 	//multithread(*fractol);
 	multithread(fractol);
 	keys_and_mouse(&fractol);
-	//menu(map);
+	menu(fractol);
 	mlx_loop(fractol.mlx.mlx_ptr);
 	return (0);
 }
